ae6c9fb0848fd43679b08b5980467e97f01eaba5
Gerigot <not@known.com>
2017-05-22 23:39:31 +0200

    added a componente and a icon library

--- src/Presentional/ToolBar.js
+++ src/Presentional/ToolBar.js
@@ -1,21 +1,32 @@
 import React from 'react'
 import { createStyleSheet } from 'jss-theme-reactor';
+import classNames from 'classnames';
 
 export const styleSheet = createStyleSheet('Toolbar', (theme) => ({
   root: {
+    alignItems: 'center',
     backgroundColor: theme.primaryColor,
-    width: '100%',
-    position: 'fixed',
-    top: 0,
+    display: 'flex',
     height: 40,
     left: 0,
+    position: 'fixed',
+    top: 0,
+    width: '100%',
   },
+  menuItem: {
+    fontSize: '1.5em',
+    '&:after': {
+      content: 'attr(data-label)',
+      fontSize: '0.75em',
+    }
+  }
 }));
 
 const ToolBar = (props, context) => {
     const classes = context.styleManager.render(styleSheet);
     return(
         <div className={classes.root}>
+          <i className={classNames("ion-edit", classes.menuItem)} data-label="nuovo messaggio"/>
         </div>
     )
 }
